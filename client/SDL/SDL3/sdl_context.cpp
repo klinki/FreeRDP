fabad8d5e50c42f9efbcd7c2ac9cf48c657b7247
David Klingenberg <dklingenberg@gmail.com>
2026-09-11 06:50:51 +0200

    [client,sdl] Route focus-synthesized input through monitor offsets

--- client/SDL/SDL3/sdl_context.cpp
+++ client/SDL/SDL3/sdl_context.cpp
@@ -1670,6 +1670,13 @@ SDL_FPoint SdlContext::screenToPixel(SDL_WindowID id, const SDL_FPoint& pos)
 	return rpos;
 }
 
+SDL_FPoint SdlContext::screenToRdp(SDL_WindowID id, const SDL_FPoint& pos)
+{
+	auto rpos = screenToPixel(id, pos);
+	applyMonitorOffset(id, rpos.x, rpos.y);
+	return rpos;
+}
+
 SDL_FPoint SdlContext::pixelToScreen(SDL_WindowID id, const SDL_FPoint& pos)
 {
 	auto w = getWindowForId(id);
